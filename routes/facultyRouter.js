abacff6747ade5e352c3d8813885e606f058e29f
AmanChandra1c <amanchandr1c7662@gmail.com>
2025-04-29 21:59:05 +0530

    faculty-studetn updated

--- routes/facultyRouter.js
+++ routes/facultyRouter.js
@@ -402,7 +402,7 @@ router.get('/students', async (req, res) => {
 
     const assignedForms = await FeedbackForm.find({
       facultyAssigned: faculty.id,
-    }).select("title formType sectionsAssigned deadline semester status");
+    }).select("title formType sectionsAssigned deadline semesters status");
 
     const feedbackData = await FeedbackResponse.find({})
 
